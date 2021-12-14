a527daf302778f7daf5801ad1a91f828a3ef157d
ShadyCone <91047866+ShadyCone@users.noreply.github.com>
2021-12-14 11:45:20 +0300

    Create Lab4.java

---
+++ Lab4.java
@@ -0,0 +1,50 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner wee = new Scanner(System.in);
+        int array[] = new int[100];
+        int prevrray[] = new int[100];
+        int position = 0, a, b;
+        int n=0;
+        int i=0;
+        System.out.println("Enter the amount of elements that you want to be in an array ");
+        b=wee.nextInt();
+        System.out.println("Enter " + b + " Element:");
+
+        for (a=0 ; a < b ; a++ )
+        {array [a] = wee.nextInt();
+         prevrray[a]= array [a];   
+            
+        }
+        
+        System.out.println("Now, please enter the number you want to delete ");
+        position= wee.nextInt();
+        
+        
+        
+        for(a=0; a<b; a++)
+        {   if (array[a]==position)
+            {   
+                n=a;
+                i+=1;
+                for ( a = n; a<b-i ; a++ )
+                { 
+                  array[a] = array [a+1];
+                
+                }
+            }
+            
+        }
+        System.out.print("The final array is : ");
+
+        for (a=0 ; a<b-i; a++ )
+        {System.out.print(" "+array[a]);}
+        
+        System.out.print("\nThe privious one was: ");
+        for (a=0 ; a<b; a++ )
+        {System.out.print(" "+prevrray[a]);}
+        
+    }
+}
